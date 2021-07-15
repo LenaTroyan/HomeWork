7abede9bbeb126d12c817cbfaf898e92eb073259
Lena Troyan <troyanlena94@gmail.com>
2021-07-15 22:06:57 +0300

    new project

---
+++ src/Unit1Task7.java
@@ -0,0 +1,11 @@
+public class Unit1Task7 {
+    public static void main (String[]  args) {
+        int a = 21;
+
+        if (a==1) {System.out.println(a+ " программист");
+        } else if (1<a && 5>a ) {
+            System.out.println(a+ " программиста");
+        } else if (5<a ) {
+            System.out.println(a+ " программистов");}
+    }
+}
