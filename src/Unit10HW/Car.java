ef12ae2dbb616b6f19c31a9711848004083597d1
Lena Troyan <troyanlena94@gmail.com>
2021-09-14 21:54:18 +0300

    Unit 9

---
+++ src/Unit10HW/Car.java
@@ -0,0 +1,49 @@
+package Unit10HW;
+
+class Car {
+    public Car(int speed, int price, String mark) {
+        this.speed = speed;
+        this.price = price;
+        this.mark = mark;
+    }
+
+    int speed;
+    int price;
+    String mark;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Car() {
+    }
+
+    public boolean start() throws myException {
+        int a = (int) (Math.random() * 20);
+       if (a % 2!=0) {
+           return true;
+        } else {
+            throw new myException("Ошибка",a);
+
+        }
+    } }
